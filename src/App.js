591d3932e7ac429adf473833623e25e10d525677
Adam Jeffery <adamjeff143@gmail.com>
2024-03-15 01:41:46 +0000

    added menu btn and all in components file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-/*import Weather from "./Weather.js";
-import React from "react";
-
-const App = () => {
-  return <>
-  <div>
-    <Weather />
-  </div>
-  </>
-}
-
-export default App*/
-
 import React from 'react';
 import Weather from './Components/Weather/Weather';
 import Main from './Components/Main/Main';
+import Menu from './Components/MenuBtn/MenuBtn';
+
 const App = () => {
   return (
     <div>
-      <div class="menu-bnt">
-        <div></div>
-      </div>
+      <Menu />
       <Main />
       <Weather/>
     </div>
