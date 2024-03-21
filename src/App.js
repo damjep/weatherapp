13ad47df2180462bf4a0b75443dae60e4a1266c2
edwin-27 <edwinjjohn@gmail.com>
2024-03-21 10:16:49 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Weather from './Components/Weather/Weather';
 import Main from './Components/Main/Main';
 import Menu from './Components/MenuBtn/Menu';
 import { Warning } from './Components/warning/warning';
-// import HourlyForecast from './Components/Hourly/HourlyForecast';
 import "./App.css";
 
 const App = () => {
