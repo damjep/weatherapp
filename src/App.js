da1f8736a366892eaaef7e040fd900c6ce7fc6cd
FadelUni <123327574+FadelUni@users.noreply.github.com>
2024-03-22 01:42:29 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Menu from './Components/MenuBtn/Menu';
 import HF from './Components/Forcast/HourlyForecast';
 import HR from './Components/Forcast/HourlyRain';
 import Sos from './Components/warning/Sos';
+import Forecast from './Components/Forecast/forecast';
 import { Warning } from './Components/warning/warning';
 import "./App.css";
 import useWeatherData from './Components/fetch/useWeatherData';
